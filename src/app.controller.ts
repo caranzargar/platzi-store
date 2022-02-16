c831b715d78687f103a7a77fdfeba976d2d98062
Carlo Andoni <caranzargar98@gmail.com>
2022-02-16 15:21:28 -0500

    Curso Fundamentos de Backend con nestJs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  getHello(): string { return 'Hola Mundo desde NestJs perú'; }
+
+  @Get('nuevo')
+  newEndPoint(): string { return 'endpoint nuevo' }
+
+  @Get('/ruta/')
+  ruta(): string { return 'con /sas/' }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes( new ValidationPipe({
+    whitelist: true,  // Eliminar los parámetros recibidos que no estén definidos en los DTO
+    forbidNonWhitelisted: true // Informar a la API que se está enviando un atributo no válido
+  }) ); //Usar el Validation pipe de forma global
   await app.listen(3000);
 }
 bootstrap();
